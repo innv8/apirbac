fe39fe0377903c5aab5cba13ff996e7b9ef64c40
rapando <samsonrapando@gmail.com>
2022-06-11 16:50:57 +0300

    also reading configs as bytes

--- configs.go
+++ configs.go
@@ -25,6 +25,20 @@ func (r *RBAC) LoadConfigs(fileName string) error {
 	return nil
 }
 
+//LoadConfigBytes is same as LoadConfigs only that it loads a file read as bytes
+func (r *RBAC) LoadConfigBytes(bytes []byte) error {
+	var err error
+	var configs RBACConfigs
+	err = json.Unmarshal(bytes, &configs)
+	if err != nil {
+		return err
+	}
+
+	r.Configs = configs
+
+	return nil
+}
+
 // SaveConfigs saves the configs to a json file for persistence/ export
 func (r *RBAC) SaveConfigs(fileName string) error {
 	configBytes, _ := json.Marshal(r.Configs)
